446ec85841aed95a6808c03ba11a02d8af1b28cd
manasij7479 <manasij7479@gmail.com>
2014-05-08 22:40:37 +0530

    search init, adjlist typedef change

---
+++ search/search.hpp
@@ -0,0 +1,14 @@
+#ifndef GRAPH_SEARCH_HPP
+#define GRAPH_SEARCH_HPP
+#include "../graph.hpp"
+namespace graph
+{
+    template<typename Graph>
+    class Search
+    {
+    public:
+        
+    private:
+    };
+}
+#endif
