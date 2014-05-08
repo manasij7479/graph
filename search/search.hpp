0750df59a78ef903ee21b6f01fb91da9bbffa4a0
manasij7479 <manasij7479@gmail.com>
2014-05-08 23:13:57 +0530

    untested search

--- search/search.hpp
+++ search/search.hpp
@@ -3,7 +3,7 @@
 #include "../graph.hpp"
 #include "../implementation/edge_traits.hpp"
 #include <map>
-
+#include <vector>
 namespace graph
 {
     template
@@ -17,13 +17,52 @@ namespace graph
         typedef typename Graph::VertexType VertexType;
         typedef typename edge_traits<VertexType>::DistanceType DistanceType;
         
-        Search():fringe()
+        Search(Graph& g_,const VertexType& s)
+            :fringe(dist),g(g_),source(s)
         {
+            dist[source]=DistanceType(0);
+            parent[source]=source;
+            fringe.put(source);
             
+            while(!fringe.empty())
+            {
+                VertexType v=fringe.get();
+                for(auto y=g.nbegin(v);y!=g.nend(y);++y)
+                {
+                    if(dist.find(y->first)==dist.end())
+                    {
+                        dist[y->first]=dist[v]+y->second;
+                        fringe.put(y->first);
+                        parent[y->first]=v;
+                    }
+                }
+            }
+            
+            
+        }
+        DistanceType distanceTo(const VertexType& v)
+        {
+            return dist[v];
+        }
+        std::vector<VertexType> getPath(const VertexType& v)
+        {
+                std::vector<VertexType> result;
+                VertexType temp=v;
+                while(parent[temp]!=temp)
+                {
+                    result.push_back(temp);
+                    temp=parent[temp];
+                }
+                result.push_back(temp);
+                std::reverse(result.begin(),result.end());
+                return result;
         }
     private:
         Container<VertexType> fringe;
         std::map<VertexType,DistanceType> dist;
+        std::map<VertexType,VertexType> parent;
+        Graph& g;
+        VertexType source;
     };
 }
 #endif
