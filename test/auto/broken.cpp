56b89d231112a239a22f139649727e8f4af4a8ae
manasij7479 <manasij7479@gmail.com>
2014-05-11 05:28:08 +0530

    build errors counted too

---
+++ test/auto/broken.cpp
@@ -0,0 +1,5 @@
+#include <iosream>
+int main{}
+(
+	std::cout<<"hodor";
+)
--- test/auto/check.sh
+++ test/auto/check.sh
@@ -5,21 +5,25 @@ c=".c"
 cplpl=".cpp"
 if [[ `echo $rp | grep .c$ -o` == "$c" ]]
 then
-	gcc $rp
-	out=`a.out`
+	gcc $rp &>/dev/null
 elif [[ `echo $rp | grep .cpp$ -o` == "$cplpl" ]]
 then
-	g++ $rp -std=c++11 
-	out=`./a.out`
-
+	g++ $rp -std=c++11 &>/dev/null
 fi
-# 	echo $expected $out
 
-if [ $expected == $out ]
+# 	echo $expected $out
+if [ $? == 0 ]
 then
-	echo '[OK]'
-	exit 0
+	out=`./a.out`
+	if [ $expected == $out ]
+	then
+		echo '[OK]'
+		exit 0
+	else
+		echo '[VALUE ERROR]'
+		exit 1
+	fi
 else
-	echo '[ERROR]'
+	echo '[BUILD ERROR]'
 	exit 1
 fi
