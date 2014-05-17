49cc4da5b26a2df7adb5524e70ba36fe68180968
manasij7479 <manasij7479@gmail.com>
2014-05-18 03:37:01 +0530

    example test

--- test/optest.cpp
+++ test/optest.cpp
@@ -1,14 +1,21 @@
 #include "testgraph.h"
 int main()
 {
+	
 	auto g=getOriginalTestGraph();
+	g.removeVertex("a");
+	g.removeEdge("d","h");
+	g.removeVertex("b");
+	g.removeVertex("g");
+	g.removeVertex("h");
+	g.removeEdge("d","f");
+	g.removeVertex("c");
+	g.removeVertex("d");
+	g.removeVertex("e");
+	g.removeVertex("f");
+	auto result=0;
 	for(auto x = g.begin();x!=g.end();++x)
-    {
-        std::cout<<"["<<x->first<<"]->";
-        for(auto y = g.nbegin(x->first);y!=g.nend(x->first);++y)
-        {
-            std::cout<<"("<< y->first<<","<< y->second<<")->";
-        }
-        std::cout<<"***\n";
-    }
+		result++;
+	
+    std::cout<<result;
 }
