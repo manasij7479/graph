0419641327a46de0cbf68c141cbd599e76727d4e
Sudipto <ttsudipto@gmail.com>
2014-06-30 20:50:09 +0530

    added intersection

--- algorithm/operations.hpp
+++ algorithm/operations.hpp
@@ -14,12 +14,30 @@ namespace graph
             result.insertVertex(i->first);
         for(auto i=g1.begin();i!=g1.end();++i)
             for(auto j=g1.nbegin(i->first);j!=g1.nend(i->first);++j)
-                if(!isAdjacent<>(result,i->first,j->first))
+                if(!isAdjacent(result,i->first,j->first))
                     result.insertEdge(i->first,j->first,j->second);
         for(auto i=g2.begin();i!=g2.end();++i)
             for(auto j=g2.nbegin(i->first);j!=g2.nend(i->first);++j)
-                if(!isAdjacent<>(result,i->first,j->first))
+                if(!isAdjacent(result,i->first,j->first))
                     result.insertEdge(i->first,j->first,j->second);
+        
+        return result;
+    }
+    
+    template<typename Graph>
+    Graph intersection(Graph g1,Graph g2)
+    {
+        Graph result;
+        for(auto i=g1.begin();i!=g1.end();++i)
+            for(auto j=g2.begin();j!=g2.end();++j)
+                if(i->first==j->first)
+                    result.insertVertex(i->first);
+        for(auto i=result.begin();i!=result.end();++i)
+            for(auto j=g1.nbegin(i->first);j!=g1.nend(i->first);++j)
+                if(isAdjacent(g2,i->first,j->first))
+                    if(!isAdjacent(result,i->first,j->first))
+                        result.insertEdge(i->first,j->first,1);
+        
         return result;
     }
 }
--- test/test.cpp
+++ test/test.cpp
@@ -1,10 +1,15 @@
 #include "../graph.hpp"
 #include "../algorithm/enumeration.hpp"
+#include "../algorithm/operations.hpp"
 #include "../util/generate.hpp"
+#include "../../../graph/util/io.hpp"
 #include<iostream>
 int main()
 {
-    auto g=graph::gen::cycle(5);
-    std::cout<<graph::degree(g,g.begin()->first);
+    auto g1=graph::gen::cycle(5);
+    auto g2=graph::gen::wheel(6);
+    auto g=graph::intersection(g1,g2);
+    graph::displayGraph(g);
+    //std::cout<<graph::degree(g,g.begin()->first);
     //std::cout<<g.begin()->first;
 }
