61ebe1ec00afb1f09e348a93b9799a7b74c2f6dd
manasij7479 <manasij7479@gmail.com>
2014-07-05 20:45:15 +0530

    New tests

---
+++ test/cyclictest.cpp
@@ -0,0 +1,10 @@
+#include "../graph.hpp"
+#include "../algorithm/predicates.hpp"
+#include "../util/generate.hpp"
+#include<iostream>
+int main()
+{
+    auto g1=graph::gen::cycle(10);
+    auto g2=graph::gen::path(10);
+    std::cout<<graph::isCyclic(g1)<<graph::isCyclic(g2);
+}
--- test/test.cpp
+++
@@ -1,9 +0,0 @@
-#include "../graph.hpp"
-#include "../algorithm/predicates.hpp"
-#include "../util/generate.hpp"
-#include<iostream>
-int main()
-{
-    auto g=graph::gen::path(100);
-    std::cout<<graph::isCyclic(g);
-}
