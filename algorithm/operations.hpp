939c7c4072d5f5b6f3492230bc59775745dd68e0
Sudipto <ttsudipto@gmail.com>
2014-07-10 22:26:16 +0530

    added cartesian product

--- algorithm/operations.hpp
+++ algorithm/operations.hpp
@@ -2,6 +2,7 @@
 #define GRAPH_ALGORITHM_OPERATIONS_HPP
 #include "predicates.hpp"
 #include "../graph.hpp"
+#include<string>
 namespace graph
 {
     template<typename Graph>
@@ -72,6 +73,25 @@ namespace graph
                     if(!isAdjacent(g1,i->first,j->first))
                         result.insertEdge(i->first,j->first,j->second);
                     
+        return result;
+    }
+    
+    std::string first(std::string s) { return s.substr(0,s.find('-')); }
+    std::string second(std::string s) { return s.substr(s.find('-')+1); }
+    template<typename Graph>
+    Graph cartesian_product(Graph g1,Graph g2)
+    {
+        Graph result;
+        for(auto i=g1.begin();i!=g1.end();++i)
+            for(auto j=g2.begin();j!=g2.end();++j)
+                result.insertVertex(i->first+"-"+j->first);
+        for(auto i=result.begin();i!=result.end();++i)
+            for(auto j=result.begin();j!=result.end();++j)
+                if(i->first!=j->first)
+                    if( (first(i->first)==first(j->first)&&isAdjacent(g2,second(i->first),second(j->first))) || (second(i->first)==second(j->first)&&isAdjacent(g1,first(i->first),first(j->first))) )
+                        if(!isAdjacent(result,i->first,j->first))
+                            result.insertEdge(i->first,j->first,1);
+        
         return result;
     }
     
