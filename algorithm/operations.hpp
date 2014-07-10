263db22342072dc945391ddad4876d77ef97bd80
Sudipto <ttsudipto@gmail.com>
2014-07-10 19:49:16 +0530

    added join

--- algorithm/operations.hpp
+++ algorithm/operations.hpp
@@ -14,14 +14,13 @@ namespace graph
             result.insertVertex(i->first);
         for(auto i=g1.begin();i!=g1.end();++i)
             for(auto j=g1.nbegin(i->first);j!=g1.nend(i->first);++j)
+                result.insertEdge(i->first,j->first,j->second);
+        for(auto i=g2.begin();i!=g2.end();++i)
+            for(auto j=g2.nbegin(i->first);j!=g2.nend(i->first);++j)
                 if(!isAdjacent(result,i->first,j->first))
                     result.insertEdge(i->first,j->first,j->second);
-                for(auto i=g2.begin();i!=g2.end();++i)
-                    for(auto j=g2.nbegin(i->first);j!=g2.nend(i->first);++j)
-                        if(!isAdjacent(result,i->first,j->first))
-                            result.insertEdge(i->first,j->first,j->second);
                         
-                        return result;
+        return result;
     }
     
     template<typename Graph>
@@ -29,17 +28,28 @@ namespace graph
     {
         Graph result;
         for(auto i=g1.begin();i!=g1.end();++i)
-            for(auto j=g2.begin();j!=g2.end();++j)
-                if(i->first==j->first)
-                    result.insertVertex(i->first);
-                for(auto i=result.begin();i!=result.end();++i)
-                    for(auto j=g1.nbegin(i->first);j!=g1.nend(i->first);++j)
-                        if(isAdjacent(g2,i->first,j->first))
-                            if(!isAdjacent(result,i->first,j->first))
-                                result.insertEdge(i->first,j->first,1);
-                            
-                            return result;
+            if(isVertex(g2,i->first))
+                result.insertVertex(i->first);
+        for(auto i=result.begin();i!=result.end();++i)
+            for(auto j=g1.nbegin(i->first);j!=g1.nend(i->first);++j)
+                if(isAdjacent(g2,i->first,j->first))
+                    if(!isAdjacent(result,i->first,j->first))
+                        result.insertEdge(i->first,j->first,1);
+                        
+        return result;
     }
+    
+    template<typename Graph>
+    Graph join(Graph g1,Graph g2)
+    {
+        Graph result=Union(g1,g2);
+        for(auto i=g1.begin();i!=g1.end();++i)
+            for(auto j=g2.begin();j!=g2.end();++j)
+                result.insertEdge(i->first,j->first,1);
+         
+        return result;
+    }
+    
     template<typename Graph>
     Graph complement(Graph g1)
     {
@@ -51,7 +61,7 @@ namespace graph
                 if(i->first!=j->first && !(isAdjacent(g1,i->first,j->first)))
                     result.insertEdge(i->first,j->first,1);
                 
-                return result;
+        return result;
     }
 }
 #endif
--- test/test.cpp
+++ test/test.cpp
@@ -3,43 +3,45 @@
 #include "../algorithm/predicates.hpp"
 #include "../algorithm/operations.hpp"
 #include "../util/generate.hpp"
+#include "../util/io.hpp"
 #include<iostream>
 int main()
 {
-      auto g=graph::gen::complete(5);
-      auto h=complement(g);
-      if(isEdgeless(h))
-		std::cout<<"1";
-	else
-		std::cout<<"0";
+    auto g=graph::gen::path(1);
+    auto h=graph::gen::cycle(5,2);
+    auto g1=graph::join(g,h);
+    graph::displayGraph(g1);
+    auto w=graph::gen::wheel(6);
+    graph::displayGraph(w);
+    
     
     /*auto g1=graph::gen::cycle(5);
-    
-    auto g=graph::gen::path(5);
-    
-    auto h=complement(g1);
-    
-    if(isComplement(g,g1))
-		std::cout<<"1";
-	else
-		std::cout<<"0";*/
+     *    
+     *    auto g=graph::gen::path(5);
+     *    
+     *    auto h=complement(g1);
+     *    
+     *    if(isComplement(g,g1))
+     *		std::cout<<"1";
+     *	else
+     *		std::cout<<"0";*/
     
     /* auto x=Union(g1,g);
-    
-    if (isComplete(x))
-		std::cout<<"1";
-	else
-		std::cout<<"0";*/
+     *    
+     *    if (isComplete(x))
+     *		std::cout<<"1";
+     *	else
+     *		std::cout<<"0";*/
     
     /*if(isEulerian(g1))
-		std::cout<<"1";
-	int k=nregular(g1);
-	if(k!=0)
-		std::cout<<k;*/
-	
+     *		std::cout<<"1";
+     *	int k=nregular(g1);
+     *	if(k!=0)
+     *		std::cout<<k;*/
+    
     /*auto g2=graph::gen::wheel(6);
-    auto g=graph::intersection(g1,g2);
-    graph::displayGraph(g);*/
+     *    auto g=graph::intersection(g1,g2);
+     *    graph::displayGraph(g);*/
     //std::cout<<graph::degree(g,g.begin()->first);
     //std::cout<<g.begin()->first;
 }
