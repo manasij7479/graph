6ef35880b334a68d9c201d7635ed206d11c5fd6e
Sudipto <ttsudipto@gmail.com>
2015-03-06 01:23:18 +0530

    Add edge coloring

--- algorithm/coloring.hpp
+++ algorithm/coloring.hpp
@@ -5,100 +5,13 @@
 #define GRAPH_ALGORITHM_COLORING_HPP
 #include "../graph.hpp"
 #include "collections.hpp"
+#include "operations.hpp"
 #include<map>
 #include<vector>
 #include<utility>
 #include<algorithm>
 namespace graph
 {
-//     template<typename Graph>
-//     class Coloring
-//     {
-//     public:
-//         typedef typename Graph::VertexType V;
-//         typedef typename Graph::EdgeType E;
-//         /**
-//          * \brief - Constructor to initialize chromatic number of the graph
-//          * **/
-//         Coloring(Graph& g_):g(g_)
-//         {
-//             chromatic_no=g.order();
-//             doneMapping=false;
-//         }
-//         /**
-//          * \brief - Returns chromtaic number of the graph
-//          * **/
-//         int getChromaticNo()
-//         {
-//             if(!doneMapping)
-//                 vertexColoring();
-//             return chromatic_no;
-//         }
-//         /**
-//          * \brief - Returns a map of vertex and corresponding chromatic number
-//          * **/
-//         std::map<V,int> getColorMap()
-//         {
-//             if(!doneMapping)
-//                 vertexColoring();
-//             return colormap;
-//         }
-//         
-//     private:
-//         Graph& g;
-//         std::map<V,int> colormap;
-//         int chromatic_no;
-//         bool doneMapping;
-//         /**
-//          * \brief - Returns a map of vertex and corresponding chromatic number for a particular
-//          * 			sequence of vertices
-//          * **/
-//         std::pair<std::map<V,int>,int> assignColor(std::vector<V> vertices)
-//         {
-//             std::map<V,int> tempmap;
-//             int ncolor=0;
-//             for(auto i=0;i<vertices.size();++i)
-//             {
-//                 std::vector<int> used;
-//                 for(auto j=g.nbegin(vertices[i]);j!=g.nend(vertices[i]);++j)
-//                 {
-//                     auto it=tempmap.find(j->first);
-//                     if(it!=tempmap.end())
-//                         used.push_back(it->second);
-//                 }
-//                 for(int c=1;c<=ncolor+1;++c)
-//                     if(std::find(used.begin(),used.end(),c)==used.end())
-//                     {
-//                         tempmap[vertices[i]]=c;
-//                         if(c==ncolor+1)
-//                             ncolor++;
-//                         break;
-//                     }
-//             }
-//             return std::make_pair(tempmap,ncolor);
-//         }
-//         /**
-//          * \brief - Determines the map of vertices and corresponding chromatic number for all possobile
-//          * 			sequence of vertices
-//          * **/
-//         void vertexColoring()
-//         {
-//             std::vector<V> vertices = VertexList(g);
-//             std::sort(vertices.begin(),vertices.end());
-//             do 
-//             {
-//                 std::pair<std::map<V,int>,int> temppair = assignColor(vertices);
-//                 if(temppair.second < chromatic_no)
-//                 {
-//                     chromatic_no = temppair.second;
-//                     colormap = temppair.first;
-//                 }
-//             } while (std::next_permutation(vertices.begin(),vertices.end()));
-//             doneMapping=true;
-//         }
-//         
-//     };
-    
     template<typename Graph, typename ColorType>
     class ColorState
     {
@@ -164,6 +77,7 @@ namespace graph
         ColorState<Graph, typename Graph::VertexType> cstate(g,tempmap,ncolor);
         return cstate;
     }
+    
     /**
      * \brief - Determines the map of vertices and corresponding chromatic number for all possobile
      *                      sequence of vertices
@@ -171,7 +85,7 @@ namespace graph
     template<typename Graph>
     ColorState<Graph, typename Graph::VertexType> minVertexColoring(Graph g)
     {
-        int min_color=g.order()+1;
+        int min_color=g.order()+1,i=0;
         ColorState<Graph, typename Graph::VertexType> min_cstate;
         auto vertices = VertexList(g);
         std::sort(vertices.begin(),vertices.end());
@@ -179,10 +93,40 @@ namespace graph
         {
             auto cstate = vertexColoring(g,vertices);
             if(cstate.noOfColorsUsed() < min_color)
+            {
+                min_color = cstate.noOfColorsUsed();
                 min_cstate = cstate;
+            }
         } while (std::next_permutation(vertices.begin(),vertices.end()));
 //         doneMapping=true;
         return min_cstate;
+    }
+    
+    template<typename Graph>
+    ColorState
+    <
+        Graph, 
+        std::pair
+        <
+            typename Graph::VertexType,
+            typename Graph::VertexType
+        >
+    >
+    minEdgeColoring(Graph g)
+    {
+        typedef typename Graph::VertexType V;
+        std::map< std::pair<V,V>, int> resultmap;
+        auto l = line(g);
+        auto cstate_l = minVertexColoring(l);
+        auto cmap=cstate_l.getColorMap();
+        for(auto i=cmap.begin();i!=cmap.end();++i)
+        {
+            auto inv = pairInvert(i->first);
+            resultmap[inv] = i->second;
+            resultmap[{inv.second,inv.first}] = i->second;
+        }
+        ColorState<Graph, std::pair<V,V>> cstate_g(g,resultmap,cstate_l.noOfColorsUsed());
+        return cstate_g;
     }
 }
 
