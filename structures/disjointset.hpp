e808a0a1cfef4d56b8a55ac4fb826dff7b1e4112
SBMama <b.subhayan@yahoo.com>
2015-03-21 05:18:58 +0530

    Changed Documentation

--- structures/disjointset.hpp
+++ structures/disjointset.hpp
@@ -1,3 +1,6 @@
+/**
+ * \brief disjointset.hpp -Header that contains disjointset structure
+ * **/
 #ifndef GRAPH_STRUCTURES_DISJOINTSET_HPP
 #define GRAPH_STRUCTURES_DISJOINTSET_HPP
 #include "parent.hpp"
@@ -9,7 +12,11 @@ namespace graph
     {
     public:
         typedef typename Graph::VertexType V;
-        
+        /**
+         * \brief - cosntructor to initialize parent array and rank array for a graph
+         * 
+         * @param Graph g - Parameter, a graph datastructure
+         * **/
         DisjointSet(Graph g)
         {
             for(auto i=g.begin();i!=g.end();++i)
@@ -18,7 +25,11 @@ namespace graph
                 rank[i->first] = 0;
             }
         }
-        
+        /**
+         * \brief - fucntion to find the root of a vertex
+         * 
+         * @param V v - Parameter, a vertex
+         * **/
         V findRoot(V v)
         {
             if(v != parent[v])
@@ -26,7 +37,17 @@ namespace graph
             else
                 return v;
         }
-        
+        /**
+         * \brief - function to find the union of roots od two vertices
+         * 
+         * if the root of two vertices are the same then the union of roots of these two vertices
+         * would e the root of either. if the roots of two vertices are no the same then the root
+         * with higher rank is made the parent of the other root.
+         * 
+         * @param V x - First Parameter, a vertex
+         * 
+         * @param V y - Second Parameter, a vertex
+         * **/
         bool Union(V x,V y)
         {
             V xroot = findRoot(x);
