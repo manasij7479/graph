19ef84ba6526c1c63fdbe3f739452b27b8a5e0e5
SBMama <b.subhayan@yahoo.com>
2015-03-21 05:35:43 +0530

    Changed Documentation

--- util/visitors.hpp
+++ util/visitors.hpp
@@ -1,24 +1,49 @@
 #ifndef GRAPH_UTIL_VISITORS_HPP
-#define GRAPH_UTIL_VISITORS_HPP
+/**
+ * \brief - Header file to store different traversal techniques
+ * **/#define GRAPH_UTIL_VISITORS_HPP
 #include "../graph.hpp"
 #include <functional>
 
 namespace graph
 {
     template<typename Graph, typename F>
+    /**
+     * \brief - function to visit all vertices of a graph
+     * 
+     * @param Graph& g - First Parameter, a graph datastructure
+     * 
+     * @param F f - Second Parameter, a callback function
+     * **/
     void VisitVertices(Graph& g, F f)
     {
         for(auto it = g.begin(); it != g.end(); ++it)
             f(it->first);
     }
-    
+    /**
+     * \brief - function to visit all neighbours of a vertex of a graph
+     * 
+     * a neighbour of a vertex is another vertex which shares an edge
+     * 
+     * @param Graph& g - First Parameter, a graph datastructure
+     * 
+     * @param Graph::VertexType v - Second Parameter, a vertex
+     * 
+     * @param F f - Third Parameter, a callback function
+     * **/
     template<typename Graph, typename F>
     void VisitNeighbours(Graph& g,typename Graph::VertexType v, F f)
     {
         for(auto it = g.nbegin(v); it != g.nend(v); ++it)
             f(it->first);
     }
-    
+    /**
+     * \brief - function to visit all edges of a graph
+     * 
+     * @param Graph& g - First Parameter, a graph datastructure
+     * 
+     * @param F f - Second Parameter, a callback function
+     * **/
     //Especially useful if the compiler is very good at optimizing lambda functions
     template<typename Graph, typename F>
     void VisitEdges(Graph& g, F f)
